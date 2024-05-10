f8c239a5e0d587687f8b729e3568d12326a190c4
Daniel Tanque <danieltanque@Daniels-MacBook-Pro.local>
2024-05-10 08:33:33 +0100

    Update to 0.7.2.0 - Most recent in Maven

--- Interactive Plugin/src/main/java/snipets/JavaSnippets.java
+++ Interactive Plugin/src/main/java/snipets/JavaSnippets.java
@@ -13,7 +13,7 @@ public class JavaSnippets implements Snippets {
 
     @Override
     public String appCoinsBillingDependency() {
-        return "\n\timplementation(\"io.catappult:android-appcoins-billing:0.7.1.0\")";
+        return "\n\timplementation(\"io.catappult:android-appcoins-billing:0.7.2.0\")";
     }
 
     @Override
